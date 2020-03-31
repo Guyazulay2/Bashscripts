afb40e7722256a50cb3453545e3d6c1f9d815a78
Guyazulay2 <59178662+Guyazulay2@users.noreply.github.com>
2020-03-31 04:42:57 +0300

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -2,51 +2,48 @@
 
 #job3
 
-echo "Updating now.."
+echo "<< Updating now >>"
 sudo apt update
 
-echo "Install java11.."
+echo "<< Install java11 >>"
 sudo apt install openjdk-11-jdk
 
-echo "Install ssh-pass"
+echo "<< Install ssh-pass >>"
 sudo apt-get install sshpass -y
 
-echo "Install python3.7.."
+echo "<< Install python3.7 >>"
 sudo apt install python3.7
 python3.7 --version
 
-echo "Make python3.7 default.."
+echo "<< Make python3.7 default >>"
 alias python==python3.7
 
-echo "Install jenkins.."
+echo "<< Install jenkins >>"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-echo "Need to update again.."
+echo "<< Need to update again >>"
 sudo apt update
-
-echo "Install jenkins.."
 sudo apt install jenkins
 systemctl status jenkins
 
 echo '#JENKINS_HOME=/var/lib/$NAME
 JENKINS_HOME=/home/jenkins">/etc/default/jenkins '
-echo "create key-gen.."
+echo "<< create key-gen >>"
 sudo ssh-keygen
-echo "create jenkins user.."
+echo "<< Creates a jenkins user and puts it in a root and sudo group >>"
 sudo useradd jenkins
 usermod -a -G sudo jenkins
 usermod -a -G root jenkins
 chmod 777 jenkins
 
-echo "Enables Port 8080.."
+echo "<< Enables Port 8080 >>"
 sudo ufw allow 8080
 
-echo "install pip3 & boto3 & paramiko.."
+echo "<< install pip3 & boto3 & paramiko >>"
 sudo apt install python3-pip
 pip3 --version
 pip install boto3
 pip install paramiko
 
-echo "install Tcpdump.. "
+echo "<< install Tcpdump >>"
 apt-get install tcpdump
