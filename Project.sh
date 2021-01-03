8f8106c20ebdbbdc0e6153088405534a7b46818b
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-01-03 09:54:20 +0200

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -12,32 +12,22 @@ echo "
 (9) | Update and upgrade
 
 (10) | Exit
-Your choice :"
+Your choice :" ; read choose
 
-read choose
-
-if [ $choose == "1" ]
-then
-	echo "< Create ssh-keygen >"
-	ssh-keygen
+if [ $choose == "1" ]; then
+	echo "< Create ssh-keygen >" ; ssh-keygen
 	sleep 0.5
-	echo "Enter the client ip >:"
-	read IP
+	echo "Enter the client ip >:" ; read IP
 	ssh-copy-id guy@$IP
 
-elif [ $choose == "2" ]
-then
+elif [ $choose == "2" ]; then
 
 	echo "Execute sudo without Password.."
-	sleep 0.5
-	echo "Enter your username >:"
-	read USER
-	echo ""$USER" ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-	echo "Done"
+	sleep 0.5 ; echo "Enter your username >:" && read USER
+	echo '"$USER" ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers ; echo "Done"
 
 
-elif [ $choose == "3" ]
-then
+elif [ $choose == "3" ]; then
 
         echo "Change host-name to slave, Enter the name >:"
         read HOST
@@ -47,48 +37,32 @@ then
         cat /home/guy/Desktop/host.txt > /etc/hostname
         reboot'
 
-elif [ $choose == "4" ]
-then
+elif [ $choose == "4" ]; then
 	echo "--- Your Ip address ---"
 	sleep 0.5
 	ip addr show enp0s3 |egrep 'inet '| awk '{print $2}'| cut -d '/' -f1
 
-elif [ $choose == "5" ]
-then
-	echo "Enter the folder name >:"
-	read FOLDER
-	mkdir $FOLDER
-	cd $FOLDER
+elif [ $choose == "5" ]; then
+	echo "Enter the folder name >:" ;read FOLDER ; mkdir $FOLDER ; cd $FOLDER
 
-	echo "Create Details file >:"
-	touch details.txt
+	echo "Create Details file >:" ; touch details.txt
 	
-	echo "Enter your details :"
-	echo "Full Name :"
-	read NAME
-	echo "Age :"
-	read AGE
-	echo "Phone Number :"
-	read PHONE
-	echo "Address :"
-	read ADD
+	echo "Enter your details :" ; echo "Full Name :" ; read NAME ; echo "Age :" ; read AGE
+	echo "Phone Number :" ; read PHONE
+	echo "Address :" ; read ADD
 
 	echo "Full Name :" $NAME > details.txt
 	echo "Age :"  $AGE >> details.txt
 	echo "Phone number :" $PHONE >> details.txt
 	echo "Adress :" $ADD >> details.txt
 	
-	echo "Enter the IP >:"
-	read IP
-	scp details.txt guy@$IP:/home/guy/Desktop/details.txt
+	echo "Enter the IP >:" ; read IP ; scp details.txt guy@$IP:/home/guy/Desktop/details.txt
 	
 	
-elif [ $choose == "6" ]
-then
+elif [ $choose == "6" ]; then
 	echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
-elif [ $choose == "7" ]
-then
+elif [ $choose == "7" ]; then
 echo "Do you want to :
 (1) | Start service
 (2) | Stop service
@@ -97,34 +71,29 @@ echo "Do you want to :
 Your choice :"
 read CHOICE
 
-if [ $CHOICE == "1" ]
-then
+if [ $CHOICE == "1" ]; then
 	echo "Enter the service name >:"
 	read NAME
 	sudo systemctl start $NAME
 
-elif [ $CHOICE == "2" ]
-then
+elif [ $CHOICE == "2" ]; then
 	echo "Enter the service name >:"
 	read NAME
 	sudo systemctl stop $NAME
 
-elif [ $CHOICE == "3" ]
-then
+elif [ $CHOICE == "3" ]; then
 	echo "Enter the service name >:"
 	read NAME
 	sudo systemctl restart $NAME
 
-elif [ $CHOICE == "4" ]
-then
+elif [ $CHOICE == "4" ]; then
 	echo "Enter the service name >:"
 	read NAME
 	sudo systemctl status $NAME
 
 fi
 
-elif [ $choose == "8" ]
-then
+elif [ $choose == "8" ]; then
 	echo "All the process"
 	sudo ps aux
 
@@ -138,15 +107,13 @@ then
 		sudo kill -9 $PID
 	done
 
-elif [ $choose == "9" ]
-then
+elif [ $choose == "9" ]; then
 	echo "Update & upgrade the machine"
 	sleep 0.5
 	sudo apt-get update
 	sudo apt-get upgrade
 
-elif [ $choose == "10" ]
-then
+elif [ $choose == "10" ]; then
 	echo "ByeBye"
 
 
